e75f1c37b5399338ca440b6940b7b4c2d912fd02
tfwright <tfwright@gmail.com>
2008-12-08 12:43:21 -0500

    test formatting for two authors

--- test/citation_test.rb
+++ test/citation_test.rb
@@ -1,7 +1,10 @@
 require(File.join(File.dirname(__FILE__), 'test_helper'))
 
 unit_tests do
-  test "truth" do
-    assert true
+  test "formats two authors sorting alphabetically joined with 'and'" do
+    citation = CiteMe::Citation.new(:authors => ["Han Solo", "James Kirk"])
+    CiteMe::Citation.publicize_methods do 
+      assert_equal "James Kirk and Han Solo", citation.formatted_authors
+    end
   end
 end
