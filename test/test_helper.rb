7d0628bba34a886ea6f506de7ac2304c86094b2e
tfwright <tfwright@gmail.com>
2008-12-08 12:42:34 -0500

    require lib, add publicize_methods helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,19 @@
 $: << File.join(File.dirname(__FILE__), '..', 'lib')
 $: << File.join(File.dirname(__FILE__))
 
+require 'citeme'
 require 'rubygems'
 require 'test/unit'
 require 'dust'
+
+# For testing private methods
+# Usage: Klass.publicize_methods { Klass.new.private_method_name }
+# http://blog.jayfields.com/2007/11/ruby-testing-private-methods.html
+class Class
+  def publicize_methods
+    saved_private_instance_methods = self.private_instance_methods
+    self.class_eval { public(*saved_private_instance_methods) }
+    yield
+    self.class_eval { private(*saved_private_instance_methods) }
+  end
+end
