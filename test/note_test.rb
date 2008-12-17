1800bae62468eb7ef63ef80f7008d37a48c3e21e
tfwright <tfwright@gmail.com>
2008-12-16 19:54:19 -0600

    updated note to use range handling logic, added tests

--- test/note_test.rb
+++ test/note_test.rb
@@ -1,7 +1,15 @@
 require(File.join(File.dirname(__FILE__), 'test_helper'))
 
 unit_tests do
-  test "truth" do
-    assert true
+
+  def to_html
+    "#{formatted_authors}, <em>#{title}</em> (#{city}: #{publisher}, #{year}), #{pages.join("-")}."
   end
+    
+  test "displays html for a footnote or endnote" do
+    note = CiteMe::Note.new(:authors => ["Han Solo", "James Kirk"], :publisher => "Random House",
+      :city => "New York", :year => 1983, :title => "Memoirs from Space", :pages => [(45..49), 51])
+    assert_equal "James Kirk and Han Solo, <em>Memoirs from Space</em> (New York: Random House, 1983), 45-49, 51.", note.to_html
+  end
+  
 end
