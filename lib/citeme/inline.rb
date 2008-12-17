f5570e51115dd06952caca2f78fbc7badbbd0a9b
tfwright <tfwright@gmail.com>
2008-12-16 19:42:05 -0600

    added tests for inline citations

--- lib/citeme/inline.rb
+++ lib/citeme/inline.rb
@@ -2,8 +2,14 @@ module CiteMe
   class Inline < CiteMe::Citation
     
     def to_html
-      "(#{formatted_authors}. #{year}, #{pages.join("-")})"
+      "(#{formatted_authors}. #{year}, #{format_ranges.join(", ")})"
     end
+    
+    private
+    
+      def format_ranges
+        pages.map { |p| p.is_a?(Range) ? "#{p.first}-#{p.last}" : p }
+      end
     
   end
 end
--- test/inline_test.rb
+++ test/inline_test.rb
@@ -1,7 +1,18 @@
 require(File.join(File.dirname(__FILE__), 'test_helper'))
 
 unit_tests do
-  test "truth" do
-    assert true
+  
+  test "displays html for an in-text citation" do
+    inline = CiteMe::Inline.new(:authors => ["Han Solo", "James Kirk"], :publisher => "Random House",
+      :city => "New York", :year => 1983, :title => "Memoirs from Space", :pages => [(45..49), 51])
+    assert_equal "(James Kirk and Han Solo. 1983, 45-49, 51)", inline.to_html
   end
+  
+  test "replaces all ranges in pages attr array with formatted string" do
+    inline = CiteMe::Inline.new(:pages => [(45..49), 51])
+    CiteMe::Inline.publicize_methods do 
+      assert_equal ["45-49", 51], inline.format_ranges
+    end
+  end
+  
 end
