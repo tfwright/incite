0a290c2900db9ca7f093e420432e21378ebfa53b
tfwright <tfwright@gmail.com>
2008-12-16 20:02:58 -0600

    removed test detritus

--- test/note_test.rb
+++ test/note_test.rb
@@ -2,10 +2,6 @@ require(File.join(File.dirname(__FILE__), 'test_helper'))
 
 unit_tests do
 
-  def to_html
-    "#{formatted_authors}, <em>#{title}</em> (#{city}: #{publisher}, #{year}), #{pages.join("-")}."
-  end
-    
   test "displays html for a footnote or endnote" do
     note = CiteMe::Note.new(:authors => ["Han Solo", "James Kirk"], :publisher => "Random House",
       :city => "New York", :year => 1983, :title => "Memoirs from Space", :pages => [(45..49), 51])
