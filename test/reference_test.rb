5ddc1596acb91180a9caa1c99072238558b00efb
tfwright <tfwright@gmail.com>
2008-12-16 20:05:43 -0600

    added tests for reference

--- test/reference_test.rb
+++ test/reference_test.rb
@@ -1,7 +1,11 @@
 require(File.join(File.dirname(__FILE__), 'test_helper'))
 
 unit_tests do
-  test "truth" do
-    assert true
+  
+  test "displays html for a reference" do
+    reference = CiteMe::Reference.new(:authors => ["Han Solo", "James Kirk"], :publisher => "Random House",
+      :city => "New York", :year => 1983, :title => "Memoirs from Space", :pages => [(45..49), 51])
+    assert_equal "James Kirk and Han Solo. 1983. <em>Memoirs from Space</em>. New York: Random House.", reference.to_html
   end
+  
 end
