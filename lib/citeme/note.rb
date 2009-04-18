42fc386c6469edab3e4662ef53682250171a600c
tfwright <tfwright@gmail.com>
2009-04-18 03:05:00 +0200

    note omits non-required datatypes cleanly

--- lib/citeme/note.rb
+++ lib/citeme/note.rb
@@ -2,9 +2,41 @@ module CiteMe
   class Note < CiteMe::Citation
     
     def to_html
-      "#{formatted_contributors(:authors)}, <em>#{source.title}</em>, trans. #{formatted_contributors(:translators)}, ed. #{formatted_contributors(:editors)}, vol. #{source.volume}, #{source.edition} ed. (#{source.city}: #{source.publisher}, #{source.year}), #{formatted_pages}."
+      relevant_attributes = %w(authors title translators editors volume edition city publisher year pages)
+      relevant_attributes.inject("") do |string, attribute|
+        string << format_segment_for(attribute)
+      end
     end
-         
+    
+    private
+    
+      def format_segment_for(attribute)
+        case attribute
+        when "authors"
+          formatted_contributors(:authors) + ", "
+        when "title"
+          "<em>#{source.title}</em>"
+        when "translators"
+          ", trans. #{formatted_contributors(:translators)}" if source.translators.any?
+        when "editors"
+          ", ed. #{formatted_contributors(:editors)}" if source.editors.any?
+        when "volume"
+          ", vol. #{source.volume}" if source.volume
+        when "edition"
+          ", #{source.edition} ed." if source.edition
+        when "city"
+          segment = " ("
+          segment << "#{source.city}: " if source.city
+          segment
+        when "publisher"
+          "#{source.publisher}, "
+        when "year"
+          "#{source.year}), "
+        when "pages"
+          "#{formatted_pages}."
+        end.to_s
+      end
+    
   end
 end
 
--- test/note_test.rb
+++ test/note_test.rb
@@ -10,4 +10,11 @@ unit_tests do
     assert_equal "James Kirk and Han Solo, <em>Memoirs from Space</em>, trans. Kooky Kookaboora, ed. Lil Rascal, vol. 1, 2 ed. (New York: Random House, 1983), 45-49, 51.", note.to_html
   end
   
+  test "omits optional fields without including related formatting and punctuation" do
+    source = CiteMe::Source.new(:authors => ["Han Solo", "James Kirk"], :publisher => "Random House",
+      :year => 1983, :title => "Memoirs from Space", :pages => [(45..49), 51])
+    note = CiteMe::Note.new(source)
+    assert_equal "James Kirk and Han Solo, <em>Memoirs from Space</em> (Random House, 1983), 45-49, 51.", note.to_html
+  end
+  
 end
